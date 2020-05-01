4ef67d66bf3b19eaf8917962721cecd6faa6b4ef
Frans911 <212281972@tut4life.ac.za>
2020-05-01 14:01:14 +0200

    Third Commit

--- Home.js
+++ Home.js
@@ -1,24 +1,43 @@
+const todoList = document.querySelector('.list'); 
+todoList.addEventListener('click', deleteCheck);
+function deleteCheck(event) {
+    const item = event.target;
+     
+        
+        if(item.classList[1] == 'fa-trash'){
+            const todoItem = item.parentElement;
+            todoItem.remove();
+        }
+        if(item.classList[1] == 'fa-check-circle'){
+            const todoItem = item.parentElement;
+            todoItem.classList.toggle('done')
+        }
+}
 function Submit() { 
     console.log("Submit....");
     if(document.forms['form']['name'].value == "" || document.forms['form']['surname'].value == "" || document.forms['form']['toDo'].value == ""){
        alert('Fill in all the fields');
        return false;
     }else{
+        
         let node = document.createElement('li');
-        let checkbox = document.createElement('input');
+        let complete = document.createElement('i');
         let erase = document.createElement('i');
 
-        erase.className = 'fas fa-trash'
+        erase.className = 'fas fa-trash';
+        erase.style.color = '#EF5350';
+        complete.className = 'fas fa-check-circle';
+        complete.style.color = '#4CAF50';
 
-        checkbox.type = 'checkbox';
-        checkbox.className = 'form-check-input'; 
-        checkbox.name = 'done'
-        checkbox.id = 'done';
+ 
+
+        
         
         let time = {hours:document.forms['form']['hours'].value,minutes:document.forms['form']['minutes'].value};
         let name = document.forms['form']['name'].value;
         let surname = document.forms['form']['surname'].value;
-        let toDo = document.forms['form']['toDo'].value; 
+        let toDo = document.forms['form']['toDo'].value;
+         
 
         console.log(document.forms['form']['name'].value)
         console.log(document.forms['form']['surname'].value)
@@ -30,14 +49,14 @@ function Submit() {
         document.getElementById('hours').value = ""; 
         document.getElementById('minutes').value = ""; 
 
-        node.appendChild(checkbox);
+        node.appendChild(complete);
+        node.appendChild(document.createTextNode(` `));
         node.appendChild(erase);
         node.appendChild(document.createTextNode(` `));
         let textNode = document.createTextNode(` ${name} ${surname} - ${toDo} - ${time.hours} : ${time.minutes}`);
         node.appendChild(textNode);
         document.getElementById('list').appendChild(node);  
-        return true;
+        
     }
-
 
 }
